c6b8e0d742ef9cb0bde99faa8d1f7ca6378a2984
Alexander Pototsky <silencemyalise@gmail.com>
2022-06-30 07:04:36 +0600

    Edit Add panel

--- WinObserver/ViewModel/ApplicationViewModel.cs
+++ WinObserver/ViewModel/ApplicationViewModel.cs
@@ -10,7 +10,7 @@ namespace WinObserver.ViewModel
 {
     public class ApplicationViewModel : INotifyPropertyChanged
     {
-        const string VERSION_APP = "Version: 0.1.5 - alpha";
+        const string VERSION_APP = "Version: 0.1.6 - alpha";
         private string _hostname;
         private string _textBlockGeneralError;
         private string _borderTextBox = "#FFABADB3";
