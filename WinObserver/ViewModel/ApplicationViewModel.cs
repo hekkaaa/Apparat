8fa428236bd2b86e4892350ef392a407a26e2d1a
Alexander Pototsky <silencemyalise@gmail.com>
2022-07-14 20:05:31 +0600

    Add DelegateCommand in TracertService and fix name version

--- WinObserver/ViewModel/ApplicationViewModel.cs
+++ WinObserver/ViewModel/ApplicationViewModel.cs
@@ -14,7 +14,7 @@ namespace WinObserver.ViewModel
 {
     public class ApplicationViewModel : INotifyPropertyChanged, IApplicationViewModel
     {
-        const string VERSION_APP = "Version: 0.1.13 - Alpha | Tester build";
+        const string VERSION_APP = "Version: 0.1.13 - Alpha";
         private string _hostname = String.Empty;
         private string _textBlockGeneralError = String.Empty;
         private string _borderTextBox = "#FFABADB3";
