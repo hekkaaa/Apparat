487973d5f36415b5b32e6a2ce7a7f819df145c69
Alexander Pototsky <silencemyalise@gmail.com>
2022-10-23 22:41:02 +0600

    fix interface ui 1

--- WinObserver/Configuration/Helpers/IconeMap.cs
+++ WinObserver/Configuration/Helpers/IconeMap.cs
@@ -2,8 +2,8 @@
 {
     public static class IconeMap
     {
-        public static string Start { get; } = "Play";
-        public static string Stop { get; } = "Stop";
+        public static string Start { get; } = "PlayOutline";
+        public static string Stop { get; } = "StopCircleOutline";
         public static string Restart { get; } = "Restart";
         public static string Trash { get; } = "Delete";
         public static string Error { get; } = @"\Resources\icons8-error.png";
